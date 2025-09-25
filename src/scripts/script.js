bd5e34d85256887cefb089495c6949f691eb4fc5
Tais Souza <tata.ssouz47@gmail.com>
2025-09-25 17:55:53 -0300

    feat: Adicionado carrosel com o uso da biblioteca swiper.

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -124,3 +124,32 @@ class TMotivo extends HTMLElement {
   }
 }
 customElements.define("t-motivos", TMotivo);
+
+// Swiper
+
+var swiper = new Swiper(".swiper", {
+  loop: true,
+  grabCursor: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 18,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 18,
+    },
+    1188: {
+      slidesPerView: 3,
+      spaceBetween: 24,
+    },
+  },
+});
